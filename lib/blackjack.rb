acd168819e4ba2156bdcc26c30bd0e7f8beba04e
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-22 01:53:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,7 +58,7 @@ def runner
   until current_card_value > 21
   current_card_value = hit?(card_total)
 end
-return display_card_total(current_card_value)
+return end_game(current_card_value)
 end
 
 
